ea81227c0de40c50151fbfa49aa076186369e973
MazeRunner <alexandertamashiro@gmail.com>
2025-04-14 22:02:02 -0300

    Pantalla principal con menú de navegación, falta el diseño

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,151 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.artículosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.verArtículoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aBMArtículosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.marcasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.verMarcasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aMBMarcaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.categoríasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.verCategoríasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aBMCategoríasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.imágenesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.verImágenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aBMImágenesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.artículosToolStripMenuItem,
+            this.marcasToolStripMenuItem,
+            this.categoríasToolStripMenuItem,
+            this.imágenesToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // artículosToolStripMenuItem
+            // 
+            this.artículosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.verArtículoToolStripMenuItem,
+            this.aBMArtículosToolStripMenuItem});
+            this.artículosToolStripMenuItem.Name = "artículosToolStripMenuItem";
+            this.artículosToolStripMenuItem.Size = new System.Drawing.Size(66, 20);
+            this.artículosToolStripMenuItem.Text = "Artículos";
+            // 
+            // verArtículoToolStripMenuItem
+            // 
+            this.verArtículoToolStripMenuItem.Name = "verArtículoToolStripMenuItem";
+            this.verArtículoToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.verArtículoToolStripMenuItem.Text = "Ver Artículo";
+            // 
+            // aBMArtículosToolStripMenuItem
+            // 
+            this.aBMArtículosToolStripMenuItem.Name = "aBMArtículosToolStripMenuItem";
+            this.aBMArtículosToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.aBMArtículosToolStripMenuItem.Text = "ABM Artículos";
+            // 
+            // marcasToolStripMenuItem
+            // 
+            this.marcasToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.verMarcasToolStripMenuItem,
+            this.aMBMarcaToolStripMenuItem});
+            this.marcasToolStripMenuItem.Name = "marcasToolStripMenuItem";
+            this.marcasToolStripMenuItem.Size = new System.Drawing.Size(57, 20);
+            this.marcasToolStripMenuItem.Text = "Marcas";
+            // 
+            // verMarcasToolStripMenuItem
+            // 
+            this.verMarcasToolStripMenuItem.Name = "verMarcasToolStripMenuItem";
+            this.verMarcasToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.verMarcasToolStripMenuItem.Text = "Ver Marcas";
+            // 
+            // aMBMarcaToolStripMenuItem
+            // 
+            this.aMBMarcaToolStripMenuItem.Name = "aMBMarcaToolStripMenuItem";
+            this.aMBMarcaToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.aMBMarcaToolStripMenuItem.Text = "AMB Marca";
+            // 
+            // categoríasToolStripMenuItem
+            // 
+            this.categoríasToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.verCategoríasToolStripMenuItem,
+            this.aBMCategoríasToolStripMenuItem});
+            this.categoríasToolStripMenuItem.Name = "categoríasToolStripMenuItem";
+            this.categoríasToolStripMenuItem.Size = new System.Drawing.Size(75, 20);
+            this.categoríasToolStripMenuItem.Text = "Categorías";
+            // 
+            // verCategoríasToolStripMenuItem
+            // 
+            this.verCategoríasToolStripMenuItem.Name = "verCategoríasToolStripMenuItem";
+            this.verCategoríasToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.verCategoríasToolStripMenuItem.Text = "Ver Categorías";
+            // 
+            // aBMCategoríasToolStripMenuItem
+            // 
+            this.aBMCategoríasToolStripMenuItem.Name = "aBMCategoríasToolStripMenuItem";
+            this.aBMCategoríasToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.aBMCategoríasToolStripMenuItem.Text = "ABM Categorías";
+            // 
+            // imágenesToolStripMenuItem
+            // 
+            this.imágenesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.verImágenToolStripMenuItem,
+            this.aBMImágenesToolStripMenuItem});
+            this.imágenesToolStripMenuItem.Name = "imágenesToolStripMenuItem";
+            this.imágenesToolStripMenuItem.Size = new System.Drawing.Size(70, 20);
+            this.imágenesToolStripMenuItem.Text = "Imágenes";
+            // 
+            // verImágenToolStripMenuItem
+            // 
+            this.verImágenToolStripMenuItem.Name = "verImágenToolStripMenuItem";
+            this.verImágenToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.verImágenToolStripMenuItem.Text = "Ver Imágen";
+            // 
+            // aBMImágenesToolStripMenuItem
+            // 
+            this.aBMImágenesToolStripMenuItem.Name = "aBMImágenesToolStripMenuItem";
+            this.aBMImágenesToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.aBMImágenesToolStripMenuItem.Text = "ABM Imágenes";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem artículosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem verArtículoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aBMArtículosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem marcasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem verMarcasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aMBMarcaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem categoríasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem verCategoríasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aBMCategoríasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem imágenesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem verImágenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aBMImágenesToolStripMenuItem;
     }
 }
 
